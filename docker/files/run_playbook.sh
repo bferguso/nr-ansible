afe140764d7e6e2f66ebfa7022eb52e47f7b201c
Ferguson, Brett CSNR:EX <brett@qedsystems.ca>
2021-11-03 15:03:32 -0700

    dev - Moved Docker config to this repo, removed bootstrap role and updated LB playbook

---
+++ docker/files/run_playbook.sh
@@ -0,0 +1,10 @@
+export ANSIBLE_COLLECTIONS_PATH=./collections
+ansible-playbook \
+	-i collections/ansible_collections/bcgov/nr/inventory/qed \
+	--extra-vars @./user_vars.yml \
+	--extra-vars @./app_vars.yml \
+	--extra-vars playbook_vars_url=https://bwa.nrs.gov.bc.ca/int/stash/projects/FFS/repos/ffs-db/raw/src/cd/playbooks/vars/playbook_vars.yml?at=refs%2Fheads%2Ffeature%2F7.4.2_new \
+	--extra-vars cdconf_target=QED \
+	--extra-vars cd_version="7.4.2-SNAPSHOT" \
+	--extra-vars db_actions="migrate" \
+	collections/ansible_collections/bcgov/nr/playbooks/liquibase.yml
