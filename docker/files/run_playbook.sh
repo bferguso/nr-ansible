88bd4e9db6deb54bad5d566a08796d7a0c94e4d1
Ferguson, Brett CSNR:EX <brett@qedsystems.ca>
2021-11-05 14:16:13 -0700

    dev - Pushed database actions into playbook prompt

--- docker/files/run_playbook.sh
+++ docker/files/run_playbook.sh
@@ -4,5 +4,4 @@ ansible-playbook \
 	--extra-vars playbook_vars_url=https://bwa.nrs.gov.bc.ca/int/stash/projects/FFS/repos/ffs-db/raw/src/cd/playbooks/vars/playbook_vars.yml?at=refs%2Fheads%2Ffeature%2F7.4.2_new \
 	--extra-vars cdconf_target=dev \
 	--extra-vars cd_version="7.4.2-SNAPSHOT" \
-	--extra-vars db_actions="migrate" \
 	collections/ansible_collections/bcgov/nr/playbooks/liquibase.yml
