8ba65b4bb11ba1029efd551feafc1a26164252a1
Ferguson, Brett CSNR:EX <brett@qedsystems.ca>
2021-11-16 09:33:03 -0800

    dev -  Removed comments

--- docker/files/init_ansible.sh
+++ docker/files/init_ansible.sh
@@ -1,4 +1,2 @@
 ansible-galaxy collection install git+https://www.github.com/bferguso/nr-ansible,feature/liquibase -p ./collections
 export ANSIBLE_COLLECTIONS_PATH=./collections
-#curl https://bferguso%40gov.bc.ca:1stTr1p1n%40Wh1l3\!\!@bwa.nrs.gov.bc.ca/int/stash/projects/FFS/repos/ffs-db/raw/src/cd/playbooks/vars/DEFAULT.yml?at=refs%2Fheads%2Ffeature%2F7.4.2_new
-#ansible-playbook --extra-vars @./playbook_vars.yml --extra-vars @./user_vars.yml collections/ansible_collections/bcgov/nr/playbooks/bootstrap.yml
