0b8cca0a326a3103a6866c593e51de51379e9571
Ferguson, Brett CSNR:EX <brett@qedsystems.ca>
2021-12-01 11:29:29 -0800

    dev - Removed all application-specific config from docker scripts

--- docker/files/run_ffs_playbook.sh
+++
@@ -1,6 +0,0 @@
-export APP_SOURCE=https://bwa.nrs.gov.bc.ca/int/stash/projects/FFS/repos/ffs-db/raw/src/cd/playbooks/vars/playbook_vars.yml?at=refs%2Fheads%2Ffeature%2F7.4.3
-export CONFIG_TARGET=qed
-export APP_VERSION=7.4.3-SNAPSHOT
-export EXTRA_VARS="--extra-vars @./app_secrets.yml"
-
-./run_playbook.sh
--- docker/files/run_playbook.sh
+++ docker/files/run_playbook.sh
@@ -2,5 +2,5 @@ export ANSIBLE_COLLECTIONS_PATH=./collections
 [ ! -f "./local_vars.yml" ] && touch ./local_vars.yml
 ansible-playbook \
 	-i collections/ansible_collections/bcgov/nr/inventory/localhost \
-	--extra-vars ./local_vars.yml \
+	--extra-vars @./local_vars.yml \
 	collections/ansible_collections/bcgov/nr/playbooks/liquibase.yml
